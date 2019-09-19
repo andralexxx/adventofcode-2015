bd5012dc13f1821dd9faab8730710a906c8d1fee
Andrii Grygoriev <AGrygori@its.jnj.com>
2019-09-19 20:20:14 +0300

    Day 19 part 1

---
+++ day-19-part-1.php
@@ -0,0 +1,81 @@
+<?php
+
+$input = <<<'INPUT'
+Al => ThF
+Al => ThRnFAr
+B => BCa
+B => TiB
+B => TiRnFAr
+Ca => CaCa
+Ca => PB
+Ca => PRnFAr
+Ca => SiRnFYFAr
+Ca => SiRnMgAr
+Ca => SiTh
+F => CaF
+F => PMg
+F => SiAl
+H => CRnAlAr
+H => CRnFYFYFAr
+H => CRnFYMgAr
+H => CRnMgYFAr
+H => HCa
+H => NRnFYFAr
+H => NRnMgAr
+H => NTh
+H => OB
+H => ORnFAr
+Mg => BF
+Mg => TiMg
+N => CRnFAr
+N => HSi
+O => CRnFYFAr
+O => CRnMgAr
+O => HP
+O => NRnFAr
+O => OTi
+P => CaP
+P => PTi
+P => SiRnFAr
+Si => CaSi
+Th => ThCa
+Ti => BP
+Ti => TiTi
+e => HF
+e => NAl
+e => OMg
+INPUT;
+
+$target = 'CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSiRnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgArSiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF';
+
+$input = <<<'INPUT'
+H => HO
+H => OH
+O => HH
+e => H
+e => O
+INPUT;
+
+$target = 'HOH';
+
+$replacements = preg_split('/\r\n|\r|\n/', $input);
+foreach ($replacements as $key => $replacement) {
+  list($from, $to) = explode(' => ', $replacement);
+  $replacements[$from][] = $to;
+  unset($replacements[$key]);
+}
+
+/**
+ * @param string $input
+ *
+ * @return array
+ */
+function replace($input = 'e') {
+  global $replacements;
+  $result = [];
+  // TODO: implement it.
+//  print_r($replacements);
+  return $result;
+}
+
+replace();
