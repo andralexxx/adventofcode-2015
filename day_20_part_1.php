8319638fda65cc18d5d3e97a430d2fe7969bbf2a
Andrii Grygoriev <AGrygori@its.jnj.com>
2019-09-20 18:48:27 +0300

    Day 20 part 1

--- day_20_part_1.php
+++ day_20_part_1.php
@@ -1,9 +1,45 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @file
+ * Created by PhpStorm.
+ * User: andralex
+ * Date: 20/9/19
+ * Time: 9:07 AM.
  */
 
-echo "Hello, World!!!";
+$target = 29000000;
+$house = 1;
+
+/**
+ * Calculate how many presents a house get.
+ *
+ * @param int $n
+ *   Number of house.
+ *
+ * @return int
+ *   Number of presents.
+ */
+function caltulatePresents($n) {
+  $summ = 0;
+  for ($i = 1; $i * $i <= $n; $i++) {
+    if ($n % $i == 0) {
+      if ($n / $i == $i) {
+        $summ += $i;
+      }
+      else {
+        $summ += $i + ($n / $i);
+      }
+    }
+  }
+  return (int) $summ * 10;
+}
+
+$result = 0;
+while ($result < $target) {
+  $result = caltulatePresents($house);
+  $house++;
+}
+$house--;
+
+printf('Result is %d for house #%d', $result, $house);
